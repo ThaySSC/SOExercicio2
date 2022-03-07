64caab3be9d79eaeeeec71c7953e333f9a64fa23
thayn <thayn@Thay>
2022-03-06 22:15:19 -0300

    Exercicio2

---
+++ src/view/Main.java
@@ -0,0 +1,38 @@
+package view;
+
+import javax.swing.JOptionPane;
+
+import controller.Killcontroller;
+
+public class Main {
+
+		public static void main(String[] args) {
+			
+		    Killcontroller cont = new Killcontroller();
+            cont.os();
+		
+        int mtprocess = 0;
+		
+		do{
+			mtprocess = Integer.parseInt(JOptionPane.showInputDialog(null, ("Deseja matar o processo por: \n 1 - Exibir \n 2 - Matar processo \n 3 - Finalizar")));
+		
+		switch (mtprocess) {
+		case 1: 
+			String process = "TASKLIST /FO TABLE";
+		    cont.readProcess(process);
+			   
+			break;
+		case 2: 
+			String process1 = "cmd.exe";
+			cont.killProcess(process1);
+			System.out.println("\n O processo foi morto!!");
+			break;
+			
+		case 3: 
+			System.out.println("\n Execução finalizada!!");
+			break;
+		}
+		}while ((mtprocess != 2) && (mtprocess != 3));
+		
+    }
+}
